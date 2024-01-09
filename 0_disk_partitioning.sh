ac297dff880a9aa1e9ce764ec26a059dab381ae4
Nicholas Georgescu <36280690+NGeorgescu@users.noreply.github.com>
2024-01-09 08:17:56 -0500

    Fixes

--- 0_disk_partitioning.sh
+++ 0_disk_partitioning.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 dev=$1
 [ -z "$dev" ] && { echo "No device specified"; exit 1; }
-echo -e ",+1G,E\n,,-\n" | sfdisk "$dev"
+
+# Start gdisk on the specified device
+gdisk "$dev" << EOF
+o
+Y
+w
+Y
+EOF
+
+# Re-run gdisk to create new partitions
+echo -e "o\nY\nn\n\n\n+1G\nEF00\nn\n\n\n\n8300\nw\nY" | gdisk "$dev"
 echo "$dev" > /tmp/partition_device
--- 3_base_install.sh
+++ 3_base_install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-pacstrap /mnt base base-devel linux linux-lts linux-firmware intel-ucode git sudo networkmanager bash-completion && \
+#!/bin/bash
+pacstrap /mnt base base-devel linux linux-lts linux-firmware intel-ucode git sudo networkmanager bash-completion z wget vim alsa-tools dosfstools exfat-utils && \
 genfstab -pU /mnt >> /mnt/etc/fstab && \
 arch-chroot /mnt /bin/bash
+pacstrap /mnt base base-devel linux linux-lts linux-firmware intel-ucode git sudo networkmanager bash-completion && \
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+# Create the sub-directory
+mkdir -p /mnt/root
+
+# Copy all .sh files from the current directory to /mnt/root
+cp *.sh /mnt/root/ && \
+
+arch-chroot /mnt /bin/bash
+
