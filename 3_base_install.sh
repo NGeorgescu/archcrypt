03f5e21f795c3ffc8207ae4799ad98cbba3cac75
Nicholas Georgescu <36280690+NGeorgescu@users.noreply.github.com>
2024-05-07 14:45:09 -0400

    Update 3_base_install.sh

--- 3_base_install.sh
+++ 3_base_install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-#!/bin/bash
 pacstrap /mnt base base-devel linux linux-lts linux-firmware intel-ucode git sudo networkmanager bash-completion z wget vim alsa-tools dosfstools exfat-utils && \
 genfstab -pU /mnt >> /mnt/etc/fstab && \
 arch-chroot /mnt /bin/bash
-pacstrap /mnt base base-devel linux linux-lts linux-firmware intel-ucode git sudo networkmanager bash-completion && \
-genfstab -pU /mnt >> /mnt/etc/fstab
-
-# Create the sub-directory
-mkdir -p /mnt/root
-
-# Copy all .sh files from the current directory to /mnt/root
-cp *.sh /mnt/root/ && \
-
-arch-chroot /mnt /bin/bash
+# pacstrap /mnt base base-devel linux linux-lts linux-firmware intel-ucode git sudo networkmanager bash-completion && \
+# genfstab -pU /mnt >> /mnt/etc/fstab
+# mkdir -p /mnt/root
+# cp *.sh /mnt/root/ && \
+# arch-chroot /mnt /bin/bash
 
