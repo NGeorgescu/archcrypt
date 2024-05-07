cec82851aaf85bef67286d2da7e45cbedd342309
Nicholas Georgescu <36280690+NGeorgescu@users.noreply.github.com>
2024-05-07 14:08:51 -0400

    Update 1_format_setup_luks.sh

--- 1_format_setup_luks.sh
+++ 1_format_setup_luks.sh
@@ -5,7 +5,7 @@ part1=$(ls ${dev}* | grep -E "${dev}p?1$")
 part2=$(ls ${dev}* | grep -E "${dev}p?2$")
 [ -z "$part1" ] || [ -z "$part2" ] && { echo "Partitions not found"; exit 1; }
 mkfs.fat -F32 "$part1" && \
-cryptsetup -v --use-random luksFormat "$part2" && \
+cryptsetup -q -v --use-random luksFormat "$part2" && \
 cryptsetup luksDump "$part2" && \
 cryptsetup luksHeaderBackup "$part2" --header-backup-file "luks_header_backup_$(date +%Y-%m-%d)" && \
 cryptsetup luksOpen "$part2" cryptroot && \
