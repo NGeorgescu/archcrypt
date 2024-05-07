fb5b1d839b715ecc2598f7f92ca572dcff697873
Nicholas Georgescu <36280690+NGeorgescu@users.noreply.github.com>
2024-05-07 14:47:35 -0400

    Update 3_base_install.sh

--- 3_base_install.sh
+++ 3_base_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 pacstrap /mnt base base-devel linux linux-lts linux-firmware intel-ucode git sudo networkmanager bash-completion z wget vim alsa-tools dosfstools exfat-utils && \
 genfstab -pU /mnt >> /mnt/etc/fstab && \
-arch-chroot /mnt /bin/bash
+# arch-chroot /mnt /bin/bash
 # pacstrap /mnt base base-devel linux linux-lts linux-firmware intel-ucode git sudo networkmanager bash-completion && \
 # genfstab -pU /mnt >> /mnt/etc/fstab
-# mkdir -p /mnt/root
-# cp *.sh /mnt/root/ && \
-# arch-chroot /mnt /bin/bash
+mkdir -p /mnt/root/ && \
+cp *.sh /mnt/root/ && \
+arch-chroot /mnt /bin/bash
 
