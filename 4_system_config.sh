4bf6b008e9d2283c7f9c23fd7e8bcb90eb0e2f65
Nicholas Georgescu <36280690+NGeorgescu@users.noreply.github.com>
2024-05-07 14:53:02 -0400

    Update 4_system_config.sh

--- 4_system_config.sh
+++ 4_system_config.sh
@@ -4,5 +4,5 @@ echo -e "LANG=en_US.UTF-8\nLANGUAGE=en_US\nLC_ALL=C" > /etc/locale.conf && \
 echo "$hostname" > /etc/hostname && \
 sed -i "/en_US.UTF-8 UTF-8/s/^#//g" /etc/locale.gen && \
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime && \
-hwclock --systohc --utc && \
+hwclock --systohc && \
 locale-gen
