735ebb66238b51496413799aab519d6facbe3134
nsg <nsgeorgescu@gmail.com>
2024-07-22 14:11:15 -0400

    edit bootloader partitions to work right

--- 6_bootloader_setup.sh
+++ 6_bootloader_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bootctl --path=/boot install && \
 echo -e "default arch\ntimeout 5" > /boot/loader/loader.conf && \
-partuuid=$(blkid -s PARTUUID -o value "$(cat /tmp/partition_device)2") && \
+partuuid=$(blkid -s PARTUUID -o value "$(findmnt -n -o SOURCE /)") && \
 echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /intel-ucode.img\ninitrd /initramfs-linux.img\noptions cryptdevice=PARTUUID=$partuuid:cryptroot root=/dev/mapper/cryptroot rw" > /boot/loader/entries/arch.conf
 
 echo "success"
