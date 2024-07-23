7d54316bc42b894b4d219523eb6c4b41a8a2e922
nsg <nsgeorgescu@gmail.com>
2024-07-23 14:56:27 -0400

    add user to sudo

--- 4_user_management.sh
+++ 4_user_management.sh
@@ -3,7 +3,7 @@
 echo "setting Root passwd"
 passwd
 read -p "Enter username: " username
-useradd -m -g users -G wheel,video "$username"
+useradd -m -g users -G wheel,video,sudo "$username"
 echo "setting $username password"
 passwd "$username"
 
