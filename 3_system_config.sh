c46ac69d917e3d9b6fb58beb09c298c68327de54
nsg <nsgeorgescu@gmail.com>
2024-07-23 14:05:01 -0400

    added /etc/hosts

--- 3_system_config.sh
+++ 3_system_config.sh
@@ -2,6 +2,7 @@
 read -p "Enter hostname: " hostname
 echo -e "LANG=en_US.UTF-8\nLANGUAGE=en_US\nLC_ALL=C" > /etc/locale.conf && \
 echo "$hostname" > /etc/hostname && \
+echo -e "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t$(hostname)" >> /etc/hosts
 sed -i "/en_US.UTF-8 UTF-8/s/^#//g" /etc/locale.gen && \
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime && \
 hwclock --systohc && \
